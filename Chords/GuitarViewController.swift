a1b7cb84742fcb1b58df32dcec82fa0e96b22476
David de Tena <daviddetenacalvo@gmail.com>
2017-02-12 21:37:42 +0100

    Included VC for guitar

---
+++ Chords/GuitarViewController.swift
@@ -0,0 +1,81 @@
+//
+//  GuitarViewController.swift
+//  Chords
+//
+//  Created by David de Tena on 12/02/2017.
+//  Copyright © 2017 David de Tena. All rights reserved.
+//
+
+import UIKit
+
+class GuitarViewController: UIViewController {
+
+    // MARK: - Properties
+    @IBOutlet weak var imgGuitarChord: UIImageView!
+    @IBOutlet weak var labelChordName: UILabel!
+    
+    
+    // MARK: - VC lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    // MARK: - Actions
+    @IBAction func chordButtonPressed(_ sender: UIButton) {
+        labelChordName.text = sender.titleLabel?.text
+        setChordImage(sender.tag)
+    }
+
+    
+    // MARK: - Utils
+    
+    func setChordImage(_ tag: Int){
+        
+        switch tag {
+        case 1:
+            imgGuitarChord.image = UIImage(named: "img_g_do")
+        case 2:
+            imgGuitarChord.image = UIImage(named: "img_g_re")
+        case 3:
+            imgGuitarChord.image = UIImage(named: "img_g_rem")
+        case 4:
+            imgGuitarChord.image = UIImage(named: "img_g_mi")
+        case 5:
+            imgGuitarChord.image = UIImage(named: "img_g_mim")
+        case 6:
+            imgGuitarChord.image = UIImage(named: "img_g_fa")
+        case 7:
+            imgGuitarChord.image = UIImage(named: "img_g_fasm")
+        case 8:
+            imgGuitarChord.image = UIImage(named: "img_g_sol")
+        case 9:
+            imgGuitarChord.image = UIImage(named: "img_g_la")
+        case 10:
+            imgGuitarChord.image = UIImage(named: "img_g_lam")
+        case 11:
+            imgGuitarChord.image = UIImage(named: "img_g_si")
+        case 12:
+            imgGuitarChord.image = UIImage(named: "img_g_sim")
+        default:
+            imgGuitarChord.image = UIImage(named: "img_mastil")
+        }
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
